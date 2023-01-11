e871a6c97930b2086ae341c77e0a8a8db2222b38
Kacper Sobczak <66113631+Kacper-So@users.noreply.github.com>
2023-01-11 22:57:43 +0100

    1.1

--- Soft/src/main.cpp
+++ Soft/src/main.cpp
@@ -22,12 +22,12 @@ using namespace std;
 #endif
 
 
-//definicje pinów
+//definicje pinow
 #define SCL                     27    //SCL pin
 #define SDA                     26    //SDA pin
 #define LED                     21    //programowalny LED
 
-//kod html wysyłany na stronę konfiguracyjną
+//kod html wysylany na strone konfiguracyjna
 const char index_html[] PROGMEM = R"rawliteral(
 <!DOCTYPE HTML>
   <html>
@@ -41,7 +41,7 @@ const char index_html[] PROGMEM = R"rawliteral(
           Ssid sieci WiFi (aktualnie %wifi_ssid%): <input type="text" name="wifi_ssid">
         </div>
         <div>
-          Hasło sieci WiFi (aktualnie %wifi_password%): <input type="number " name="wifi_password">
+          Haslo sieci WiFi (aktualnie %wifi_password%): <input type="number " name="wifi_password">
         </div>
         <div>
           Ip brokera MQTT (aktualnie %ip_mqtt%): <input type="number " name="ip_mqtt">
@@ -52,12 +52,12 @@ const char index_html[] PROGMEM = R"rawliteral(
     </body>
   </html>)rawliteral";
 
-//dane dostępowe do sieci ESP
+//dane dostepowe do sieci ESP
 AsyncWebServer server(80);
 const char* ap_ssid = "Moduł IMU";
 const char* ap_password = "123456789";
 
-//dane dostępowe do sieci WiFi
+//dane dostepowe do sieci WiFi
 String wifi_ssid;
 String wifi_password;
 String ip_mqtt;
@@ -74,7 +74,7 @@ hw_timer_t * IMU_timer = NULL;
 
 LSM9DS1 imu;
 
-struct settings_container{//kontener dla ustawień urządzenia
+struct settings_container{//kontener dla ustawien urzadzenia
 public:
   float stream_frq;       //częstotliwość zbierania i wysyłania danych [Hz] maksymalnie 80 
   int gyro_scale;         //skala żyroskopu  [245,500,2000]
@@ -96,7 +96,7 @@ settings_container settings;
 String readFile(fs::FS &fs, const char * path){   //wczytaj string z pliku
   File file = fs.open(path, "r");
   if (!file || file.isDirectory()){
-    Serial.println("błąd zapisu do pliku");
+    Serial.println("blad zapisu do pliku");
     return String();
   }
   String fileContent;
@@ -110,16 +110,16 @@ String readFile(fs::FS &fs, const char * path){   //wczytaj string z pliku
 void writeFile(fs::FS &fs, const char * path, const char * message){    //dodaj string do pliku
   File file = fs.open(path, "w");
   if(!file){
-    Serial.println("błąd przy otwieraniu pliku do zapisu");
+    Serial.println("blad przy otwieraniu pliku do zapisu");
     return;
   }
   if(!file.print(message)){
-    Serial.println("błąd zapisu do pliku");
+    Serial.println("blad zapisu do pliku");
   }
   file.close();
 }
 
-String send_current_credentials(const String& x){    //wyślij aktualnie zapamiętane dane dostępowe do sieci na serwer
+String send_current_credentials(const String& x){    //wyslij aktualnie zapamietane dane dostepowe do sieci na serwer
   if (x == "wifi_ssid"){
     return readFile(SPIFFS, "/wifi_ssid.txt");
   } else if (x == "wifi_password"){
@@ -138,15 +138,15 @@ void config_page_setup(){   //funkcja odpowiedzialna za utrzymanie strony konfig
   WiFi.mode(WIFI_AP_STA);
   WiFi.softAP(ap_ssid,ap_password);
   IPAddress IP = WiFi.softAPIP();
-  Serial.printf("IP adrest strony konfiguracyjnej: %s \n", IP.toString().c_str()); //wyświetla ip strony konfiguracyjnej
+  Serial.printf("IP adrest strony konfiguracyjnej: %s \n", IP.toString().c_str()); //wyswietla ip strony konfiguracyjnej
   server.begin();
 
-  //wysyłanie kodu html oraz aktualnie zapamiętane dane dostępowe do sieci na żądanie serwera
+  //wysylanie kodu html oraz aktualnie zapamietane dane dostepowe do sieci na zadanie serwera
   server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
     request->send_P(200, "text/html", index_html, send_current_credentials);
   });
 
-  //odebranie danych od urzytkownika oraz zapisanie ich w pamięci nieulotnej
+  //odebranie danych od urzytkownika oraz zapisanie ich w pamieci nieulotnej
   server.on("/get", HTTP_GET, [] (AsyncWebServerRequest *request) {
     String inputMessage;
     if (request->hasParam("wifi_ssid")) {
@@ -174,9 +174,9 @@ void config_page_setup(){   //funkcja odpowiedzialna za utrzymanie strony konfig
   server.begin();
 }
 
-//odbiera wiadomość wysłaną przy pomocy MQTT przez użytkownika, 
-//zapisuje temat oraz treść do zmiennych globalnych
-//odblokowuje proces analizujący odebraną wiadomość
+//odbiera wiadomosc wyslana przy pomocy MQTT przez uzytkownika, 
+//zapisuje temat oraz tresc do zmiennych globalnych
+//odblokowuje proces analizujacy odebrana wiadomosc
 void MQTTcallback(char* topic, byte* payload, unsigned int length){
   if(length <=64){
     MQTT_topic = topic;
@@ -187,22 +187,22 @@ void MQTTcallback(char* topic, byte* payload, unsigned int length){
     MQTT_payload[i] = NULL;
     xSemaphoreGive(sema_MQTT_Parser);
   } else {
-    Serial.println("Błędne polecenie");
-    MQTTclient.publish("Debug", "Błędne polecenie");
+    Serial.println("Bledne polecenie");
+    MQTTclient.publish("Debug", "Bledne polecenie");
   }
 }
 
-//strażnik połączenia
-//nawiązuje połączenie z siecią WiFi oraz brokerm MQTT
-//utrzymuje połączenie oraz ponawia w przypadku zerwania
+//straznik polaczenia
+//nawiazuje polaczenie z siecia WiFi oraz brokerm MQTT
+//utrzymuje połaczenie oraz ponawia w przypadku zerwania
 void connection_watchdog(void *parameter){
   while (1){
-    //sprawdza połączenie z siecią WiFi
+    //sprawdza polaczenie z siecia WiFi
     if (wifiClient.connected() && WiFi.status() == WL_CONNECTED){
       MQTTclient.loop();
-    } else { //w sytuacji braku połączenia następuje próba jego nawiązania w pętli co 5 sekund
+    } else { //w sytuacji braku polaczenia nastepuje proba jego nawiazania w petli co 5 sekund
       settings.ON_OFF = false;
-      Serial.println("Połączenie nieudane, możliwa potrzeba nowych danych dostępu do sieci");
+      Serial.println("Polaczenie nieudane, mozliwa potrzeba nowych danych dostepu do sieci");
       if (WiFi.status() != WL_CONNECTED){
         while (WiFi.status() != WL_CONNECTED){
           wifi_ssid = readFile(SPIFFS, "/wifi_ssid.txt");
@@ -210,16 +210,16 @@ void connection_watchdog(void *parameter){
           WiFi.begin(wifi_ssid.c_str(), wifi_password.c_str());
           vTaskDelay(5000 / portTICK_PERIOD_MS);
         }
-        Serial.println("Połączenie z WiFi udane");
+        Serial.println("Polaczenie z WiFi udane");
       }
-      //po zapewnieniu połączenia z siecią WiFi następuje próba połączenia z brokerek MQTT w pętli co 5 sekund
+      //po zapewnieniu polaczenia z siecia WiFi nastepuje proba połaczenia z brokerem MQTT w petli co 5 sekund
       while (!MQTTclient.connected()){
         ip_mqtt = readFile(SPIFFS, "/ip_mqtt.txt");
         MQTTclient.setServer(ip_mqtt.c_str(), 1883);
         MQTTclient.connect("user");
         vTaskDelay(5000 / portTICK_PERIOD_MS);
       }
-      MQTTclient.publish("Debug", "Połączenie z MQTT udane");
+      MQTTclient.publish("Debug", "Polaczenie z MQTT udane");
       MQTTclient.setCallback(MQTTcallback);
       MQTTclient.subscribe("Ustawienia");
     }
@@ -227,7 +227,7 @@ void connection_watchdog(void *parameter){
   }
 }
 
-//Proces wybudzany w momencie otrzymania wiadomości od użytkownika
+//Proces wybudzany w momencie otrzymania wiadomosci od uzytkownika
 void parse_MQTT(void *parameter){
   char argument[14];
   char function[50];
@@ -235,7 +235,7 @@ void parse_MQTT(void *parameter){
     xSemaphoreTake(sema_MQTT_Parser, portMAX_DELAY); 
     if (MQTT_topic == "Ustawienia"){
       if (MQTT_payload[0] != NULL){
-        //analiza wiadomości, rozdział na polecenie i argument
+        //analiza wiadomosci, rozdzial na polecenie i argument
         char* iter_MQTT_payload = MQTT_payload;
         char* iter_function = function;
         char* iter_argument = argument;
@@ -250,48 +250,48 @@ void parse_MQTT(void *parameter){
           iter_MQTT_payload++;
           iter_argument++;
         }
-        //w zależności od treści wiadomości podejmowane są działania
-        //polecenie zmieniające częstotliwość zbierania i wysyłania danych
+        //w zaleznosci od tresci wiadomosci podejmowane są dzialania
+        //polecenie zmieniające częstotliwosc zbierania i wysylania danych
         if (String(function) == "czestotliwosc"){
           if (atof(argument) > 0 && atof(argument) <= 80){
-            MQTTclient.publish("Debug", "Zmieniono częstotliwość");
+            MQTTclient.publish("Debug", "Zmieniono czestotliwosc");
             settings.stream_frq = atof(argument);
-            //zmiana paremetów przerwania czasowego które realizuje wysyłanie danych
+            //zmiana paremetow przerwania czasowego ktore realizuje wysylanie danych
             timerAlarmWrite(IMU_timer, (1 / settings.stream_frq) * 1000000, true);
           } else {
-            MQTTclient.publish("Debug", "Maksymalna czętotliwość to 80Hz");
+            MQTTclient.publish("Debug", "Maksymalna czetotliwosc to 80Hz");
           }
-        //polecenie zmieniające skalę żyroskopu
+        //polecenie zmieniajace skale zyroskopu
         } else if (String(function) == "Skala_zyroskopu"){
           if (atoi(argument) == 245 || atoi(argument) == 500 || atoi(argument) == 2000){
-            MQTTclient.publish("Debug", "Zmieniono skalę żyroskopu");
+            MQTTclient.publish("Debug", "Zmieniono skalę zyroskopu");
             settings.gyro_scale = atoi(argument);
-            //zmiana ustawień IMU
+            //zmiana ustawien IMU
             imu.settings.gyro.scale = settings.gyro_scale;
           } else {
-            MQTTclient.publish("Debug", "Dostępne skalowanie dla żyroskopu : 245,500,2000");
+            MQTTclient.publish("Debug", "Dostepne skalowanie dla zyroskopu : 245,500,2000");
           }
-        //polecenie zmieniające skalę akcelerometru
+        //polecenie zmieniajace skale akcelerometru
         } else if (String(function) == "Skala_akcelerometru"){
           if (atoi(argument) == 2 || atoi(argument) == 4 || atoi(argument) == 8 || atoi(argument) == 16){
-            MQTTclient.publish("Debug", "Zmieniono skalę akcelerometru");
+            MQTTclient.publish("Debug", "Zmieniono skale akcelerometru");
             settings.accel_scale = atoi(argument);
-            //zmiana ustawień IMNU
+            //zmiana ustawien IMU
             imu.settings.accel.scale = settings.accel_scale;
           } else {
-            MQTTclient.publish("Debug", "Dostępne skalowanie dla akcelerometru : 2,4,8,16");
+            MQTTclient.publish("Debug", "Dostwpne skalowanie dla akcelerometru : 2,4,8,16");
           }
-        //polecenie zmieniające skalę magnetometru
+        //polecenie zmieniające skalw magnetometru
         } else if (String(function) == "Skala_magnetometru"){
           if (atoi(argument) == 4 || atoi(argument) == 8 || atoi(argument) == 12 || atoi(argument) == 16){
-            MQTTclient.publish("Debug", "Zmieniono skalę magnetometru ");
+            MQTTclient.publish("Debug", "Zmieniono skale magnetometru ");
             settings.mag_scale = atoi(argument);
-            //zmiana ustawień IMU
+            //zmiana ustawien IMU
             imu.settings.mag.scale = settings.mag_scale;
           } else {
-            MQTTclient.publish("Debug", "Dostępne skalowanie magnetometru : 4,8,12,16");
+            MQTTclient.publish("Debug", "Dostepne skalowanie magnetometru : 4,8,12,16");
           }
-        //polecenie aktywujące strumieniowaie danych
+        //polecenie aktywujace strumieniowaie danych
         } else if (String(function) == "ON/OFF"){
           if(String(argument) == "ON"){
             settings.ON_OFF = true;
@@ -300,20 +300,43 @@ void parse_MQTT(void *parameter){
             settings.ON_OFF = false;
             MQTTclient.publish("Debug", "IMU OFF");
           }
-        //polecenie wyświetlające aktualne ustawienia
+        //polecenie wyswietlajace aktualne ustawienia
         } else if (String(function) == "Ustawienia"){
           MQTTclient.publish("Debug", String(settings.stream_frq).c_str());
           MQTTclient.publish("Debug", String(settings.gyro_scale).c_str());
           MQTTclient.publish("Debug", String(settings.accel_scale).c_str());
           MQTTclient.publish("Debug", String(settings.mag_scale).c_str());
           MQTTclient.publish("Debug", String(settings.ON_OFF).c_str());
-        //polecenie rozpoczynające proces kalibracji IMU
+        //polecenie rozpoczynajace proces kalibracji IMU
         } else if (String(function) == "Kalibruj"){
           imu.calibrate();
           imu.calibrateMag();
-          MQTTclient.publish("Debug", "Kalibracja zakończona");
+          MQTTclient.publish("Debug", "Kalibracja zakonczona");
+        } else if (String(function) == "pasmo_zyroskopu"){
+          if (atoi(argument) == 0 || atoi(argument) == 1 || atoi(argument) == 2 || atoi(argument) == 3){
+            MQTTclient.publish("Debug", "Zmieniono pasmo zyroskopu");
+            imu.settings.gyro.bandwidth = atoi(argument);
+          } else {
+            MQTTclient.publish("Debug","Dostepne opcje to : 0, 1, 2, 3");
+          }
+        } else if (String(function) == "odciecie_zyroskopu"){
+          if (atoi(argument) == 0 || atoi(argument) == 1 || atoi(argument) == 2 || atoi(argument) == 3 ||
+              atoi(argument) == 4 || atoi(argument) == 5 || atoi(argument) == 6 || atoi(argument) == 7 ||
+              atoi(argument) == 8 || atoi(argument) == 9){
+            MQTTclient.publish("Debug", "Zmieniono odciecie zyroskopu");
+            imu.settings.gyro.HPFCutoff = atoi(argument);
+          } else {
+            MQTTclient.publish("Debug", "Dostepne opcje to : 0, 1, 2, 3, 4, 5, 6, 7, 8, 9");
+          }
+        } else if (String(function) == "pasmo_akcelerometru"){
+          if (atoi(argument) == 0 || atoi(argument) == 1 || atoi(argument) == 2 || atoi(argument) == 3){
+            MQTTclient.publish("Debug", "Zmieniono pasmo akcelerometru");
+            imu.settings.accel.highResBandwidth = atoi(argument);
+          } else {
+            MQTTclient.publish("Debug","Dostepne opcje to : 0, 1, 2, 3");
+          }
         } else {
-          MQTTclient.publish("Debug","Błędne polecenie");
+          MQTTclient.publish("Debug","Błedne polecenie");
         }
         memset(function, NULL, 50);
         memset(argument, NULL, 14);
@@ -324,14 +347,14 @@ void parse_MQTT(void *parameter){
   }
 }
 
-//przerwanie czasowe odblokowujące proces wysyłający dane
+//przerwanie czasowe odblokowujace proces wysylajacy dane
 void IRAM_ATTR IMU_timer_callback(){
   if (settings.ON_OFF == true){
     xSemaphoreGive(sema_data_rdy);
   }
 }
 
-//proces wysyłający dane
+//proces wysylajacy dane
 void send_data(void *parameter){
   while (1){
     xSemaphoreTake(sema_data_rdy, portMAX_DELAY);
@@ -351,10 +374,10 @@ void send_data(void *parameter){
   }
 }
 
-//funkcja setup, wykonuje się jednorazowo po rozpoczęciu pracy urządzenia
+//funkcja setup, wykonuje się jednorazowo po rozpoczeciu pracy urzadzenia
 void setup() {
   Serial.begin(115200);
-  //aktywacja punktu dostępu i strony konfiguracyjnej
+  //aktywacja punktu dostepu i strony konfiguracyjnej
   config_page_setup();
   Wire.begin(SDA,SCL);
   //inicjalizacja IMU
@@ -366,6 +389,8 @@ void setup() {
     imu.settings.gyro.sampleRate = 4;
     imu.settings.accel.sampleRate = 4;
     imu.settings.mag.sampleRate = 7;
+    imu.settings.accel.highResEnable = true;
+    imu.settings.gyro.HPFEnable = true;
   }
   sema_MQTT_Parser = xSemaphoreCreateBinary();
   sema_data_rdy = xSemaphoreCreateBinary();
@@ -374,11 +399,11 @@ void setup() {
   timerAttachInterrupt(IMU_timer, &IMU_timer_callback, true);
   timerAlarmWrite(IMU_timer, (1 / settings.stream_frq) * 1000000, true);
   timerAlarmEnable(IMU_timer);
-  //inicjalizacja procesu strażnika połączenia
+  //inicjalizacja procesu straznika polaczenia
   xTaskCreatePinnedToCore(connection_watchdog, "connection_watchdog", 2500, NULL, 1, NULL, app_cpu);
-  //inicjalizacja procesu analizującego przychodzące wiadomości
+  //inicjalizacja procesu analizujacego przychodzace wiadomosci
   xTaskCreatePinnedToCore(parse_MQTT, "parse_MQTT", 2500, NULL, 1, NULL, app_cpu);
-  //inicjalizacja procesu wysyłającego dane
+  //inicjalizacja procesu wysylającego dane
   xTaskCreatePinnedToCore(send_data, "send_data", 2500, NULL, 1, NULL, app_cpu);
 
   vTaskDelete(NULL);
